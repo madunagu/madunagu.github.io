b2d371fffc7abfbe8bbf4853015d684b35a986e0
Ekene Madunagu <ekenemadunagu@gmail.com>
2024-07-24 16:31:58 +0100

    removed dist again

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 import Lenis from "@studio-freight/lenis";
 import { gsap } from "gsap";
-import { PixiPlugin } from "gsap/PixiPlugin";
+// import { PixiPlugin } from "gsap/PixiPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import SplitType from "split-type";
-import { NoToneMapping } from "three";
+// import { ScrollSmoother } from "gsap/ScrollSmoother";
+// import SplitType from "split-type";
+// import { NoToneMapping } from "three";
 // import "./animation";
 
-console.log("Hello from Vite");
+// console.log("Hello from Vite");
 
-gsap.registerPlugin(ScrollTrigger, PixiPlugin);
-const lenis = new Lenis();
-lenis.on("scroll", ScrollTrigger.update);
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
-gsap.ticker.lagSmoothing(0);
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
+// const lenis = new Lenis({});
+// lenis.on("scroll", ScrollTrigger.update);
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000);
+// });
+// gsap.ticker.lagSmoothing(0);
 
 
 // function onRaf(time) {
@@ -24,6 +25,11 @@ gsap.ticker.lagSmoothing(0);
 
 // requestAnimationFrame(onRaf);
 
+ScrollSmoother.create({
+  smooth: 1.5,
+  effects: true,
+  smoothTouch: false
+});
 
 const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 let isDarkMode = darkModeMediaQuery.matches;
@@ -31,14 +37,14 @@ let isDarkMode = darkModeMediaQuery.matches;
 
 let themeSwitcher = document.getElementById("change-theme");
 let cursor = document.getElementById("cursor");
-let hand = document.getElementById("hand");
+// let hand = document.getElementById("hand");
 let movingAlong = gsap.utils.toArray(".moving-along");
-let skillSlider = document.getElementById("skill-slider");
+// let skillSlider = document.getElementById("skill-slider");
 let portfolioCards = gsap.utils.toArray(".portfolio .portfolio-card");
 let buttonCursor = gsap.utils.toArray(".button-cursor");
-let focusCursor = gsap.utils.toArray(".focus-cursor");
-let magneticLinks = gsap.utils.toArray(".magnetic");
-let expertiseImages = gsap.utils.toArray(".expertise .img-cont")
+// let focusCursor = gsap.utils.toArray(".focus-cursor");
+// let magneticLinks = gsap.utils.toArray(".magnetic");
+// let expertiseImages = gsap.utils.toArray(".expertise .img-cont")
 
 const moveFollowingCursor = (e) => {
   const { clientX, clientY } = e;
@@ -54,96 +60,96 @@ const moveFollowingCursor = (e) => {
 }
 
 
-const magnetize = (e) => {
-  // Calculate mouse position relative to the button
-  console.log(e);
-  const button = e.target;
-  const boundingRect = button.getBoundingClientRect();
-  const mousePosX = e.x - boundingRect.left; // X position
-  const mousePosY = e.y - boundingRect.top;  // Y position
+// const magnetize = (e) => {
+//   // Calculate mouse position relative to the button
+//   console.log(e);
+//   const button = e.target;
+//   const boundingRect = button.getBoundingClientRect();
+//   const mousePosX = e.x - boundingRect.left; // X position
+//   const mousePosY = e.y - boundingRect.top;  // Y position
 
-  const x = (mousePosX - boundingRect.width / 2) * 0.4;
-  const y = (mousePosY - boundingRect.height / 2) * 0.4;
+//   const x = (mousePosX - boundingRect.width / 2) * 0.4;
+//   const y = (mousePosY - boundingRect.height / 2) * 0.4;
 
 
-  console.log('magnetizing... x:' + x + ' y:' + y);
-  // Use GSAP (GreenSock Animation Platform) to animate the button
-  // This creates a 'magnetic' effect where the button moves towards the mouse
-  gsap.to(button, {
-    '--x': `${x}px`,
-    '--y': `${y}px`,// Move vertically towards mouse
-    duration: 0.8, // Duration of the animation
-    ease: 'power3.out', // Easing function for smooth animation
-  });
-};
+//   console.log('magnetizing... x:' + x + ' y:' + y);
+//   // Use GSAP (GreenSock Animation Platform) to animate the button
+//   // This creates a 'magnetic' effect where the button moves towards the mouse
+//   gsap.to(button, {
+//     '--x': `${x}px`,
+//     '--y': `${y}px`,// Move vertically towards mouse
+//     duration: 0.8, // Duration of the animation
+//     ease: 'power3.out', // Easing function for smooth animation
+//   });
+// };
 
-const deMagnetize = (e) => {
-  // Animate the button back to its original position
-  console.log('demagnetizing ');
-  const button = e.target;
+// const deMagnetize = (e) => {
+//   // Animate the button back to its original position
+//   console.log('demagnetizing ');
+//   const button = e.target;
 
-  console.log('demagnetizing...');
-  gsap.to(button, {
-    '--x': `0px`,
-    '--y': `0px`,
-    duration: 0.8, // Duration of the animation
-    ease: 'elastic.out(1,0.3)' // Easing function for a 'springy' return
-  });
-};
+//   console.log('demagnetizing...');
+//   gsap.to(button, {
+//     '--x': `0px`,
+//     '--y': `0px`,
+//     duration: 0.8, // Duration of the animation
+//     ease: 'elastic.out(1,0.3)' // Easing function for a 'springy' return
+//   });
+// };
 
-const changeFollowingCursorToHand = (e) => {
+// const changeFollowingCursorToHand = (e) => {
 
-  console.log('changing cursor...');
-  gsap.to(movingAlong, {
-    '--w': '0rem',
-    '--s': '5rem',
-    '--o': 1,
-    '--r': '90deg',
-    duration: .4,
-    ease: 'sine.out',
-  });
-};
+//   console.log('changing cursor...');
+//   gsap.to(movingAlong, {
+//     '--w': '0rem',
+//     '--s': '5rem',
+//     '--o': 1,
+//     '--r': '90deg',
+//     duration: .4,
+//     ease: 'sine.out',
+//   });
+// };
 
-const changeFollowingCursorToBall = (e) => {
+// const changeFollowingCursorToBall = (e) => {
 
-  console.log('reversing cursor... ');
-  gsap.to(movingAlong, {
-    '--w': '5rem',
-    '--s': '1rem',
-    '--o': 0,
+//   console.log('reversing cursor... ');
+//   gsap.to(movingAlong, {
+//     '--w': '5rem',
+//     '--s': '1rem',
+//     '--o': 0,
 
-    '--r': '0deg',
-    // '--y': `${y}%`,
-    duration: .4,
-    ease: 'sine.out',
-  });
-};
+//     '--r': '0deg',
+//     // '--y': `${y}%`,
+//     duration: .4,
+//     ease: 'sine.out',
+//   });
+// };
 
-const shrinkCursor = (e) => {
+// const shrinkCursor = (e) => {
 
-  console.log('shrinking cursor...');
-  gsap.to(movingAlong, {
-    '--w': '0rem',
-    '--s': '5rem',
-    // '--o': 1,
-    // '--r': '90deg',
-    duration: .4,
-    ease: 'sine.out',
-  });
-};
+//   console.log('shrinking cursor...');
+//   gsap.to(movingAlong, {
+//     '--w': '0rem',
+//     '--s': '5rem',
+//     // '--o': 1,
+//     // '--r': '90deg',
+//     duration: .4,
+//     ease: 'sine.out',
+//   });
+// };
 
-const expandCursor = (e) => {
+// const expandCursor = (e) => {
 
-  console.log('expanding cursor...');
-  gsap.to(movingAlong, {
-    '--w': '5rem',
-    // '--s': '5rem',
-    // '--o': 1,
-    // '--r': '90deg',
-    duration: .4,
-    ease: 'sine.out',
-  });
-};
+//   console.log('expanding cursor...');
+//   gsap.to(movingAlong, {
+//     '--w': '5rem',
+//     // '--s': '5rem',
+//     // '--o': 1,
+//     // '--r': '90deg',
+//     duration: .4,
+//     ease: 'sine.out',
+//   });
+// };
 
 
 const setThemeMode = (mode) => {
@@ -169,17 +175,17 @@ const toggleThemeMode = () => {
   });
 }
 
-let scrollTween = gsap.to(portfolioCards, {
-  xPercent: -100 * (portfolioCards.length),
-  ease: 'none',
-  scrollTrigger: {
-    trigger: '.custom-scroll',
-    pin: true,
-    scrub: 1,
-    // start: "top 40%",
-    end: "+=3000"
-  }
-});
+// let scrollTween = gsap.to(portfolioCards, {
+//   xPercent: -100 * (portfolioCards.length),
+//   ease: 'none',
+//   scrollTrigger: {
+//     trigger: '.custom-scroll',
+//     pin: true,
+//     scrub: 1,
+//     // start: "top 40%",
+//     end: "+=3000"
+//   }
+// });
 
 // let expertiseTween = gsap.from(expertiseImages, {
 //   ease: 'sine.out',
@@ -191,17 +197,17 @@ let scrollTween = gsap.to(portfolioCards, {
 //   }
 // });
 
-let skillSliderTween = gsap.to(skillSlider, {
-  ease: 'none',
-  marginLeft: '-100%',
-  scrollTrigger: {
-    trigger: '#skill-slider',
-    // pin: true,
-    scrub: 1,
-    start: "top 90%",
-    // end: "+=3000"
-  }
-});
+// let skillSliderTween = gsap.to(skillSlider, {
+//   ease: 'none',
+//   marginLeft: '-100%',
+//   scrollTrigger: {
+//     trigger: '#skill-slider',
+//     // pin: true,
+//     scrub: 1,
+//     start: "top 90%",
+//     // end: "+=3000"
+//   }
+// });
 
 // const splitTypes = document.querySelectorAll(".body-text-large");
 // splitTypes.forEach((char, i) => {
@@ -221,36 +227,6 @@ let skillSliderTween = gsap.to(skillSlider, {
 
 
 
-
-const rotateCard = (e) => {
-  const text = new SplitType(char, { types: 'words' });
-  console.log(text);
-  gsap.from(text.words, {
-    scrollTrigger: {
-      trigger: char,
-      start: 'top 64%',
-      end: 'top 20%',
-      scrub: true,
-    },
-    opacity: 0.2,
-    // stagger: 0.1,
-  });
-};
-const deRotateCard = (char, i) => {
-  const text = new SplitType(char, { types: 'words' });
-  console.log(text);
-  gsap.from(text.words, {
-    scrollTrigger: {
-      trigger: char,
-      start: 'top 64%',
-      end: 'top 20%',
-      scrub: true,
-    },
-    opacity: 0.2,
-    stagger: 0.1,
-  });
-};
-let constrain = 20;
 
 function transforms(x, y, el, scale) {
   let box = el.getBoundingClientRect();
@@ -316,8 +292,6 @@ function resetElement(el) {
 //   card.addEventListener("mouseleave", (e) => {
 //     resetElement(portfolioImage);
 //     resetElement(overlayPanel);
-//     // portfolioImage.style.transform = 'none';
-//     // overlayPanel.style.transform = 'none';
 //   });
 // });
 
@@ -343,8 +317,8 @@ function resetElement(el) {
 setThemeMode(isDarkMode);
 
 window.addEventListener("mousemove", moveFollowingCursor);
-buttonCursor.forEach(elem => elem.addEventListener("mouseenter", changeFollowingCursorToHand));
-buttonCursor.forEach(elem => elem.addEventListener("mouseleave", changeFollowingCursorToBall));
+// buttonCursor.forEach(elem => elem.addEventListener("mouseenter", changeFollowingCursorToHand));
+// buttonCursor.forEach(elem => elem.addEventListener("mouseleave", changeFollowingCursorToBall));
 // focusCursor.forEach(elem => elem.addEventListener("mouseenter", shrinkCursor));
 // focusCursor.forEach(elem => elem.addEventListener("mouseleave", expandCursor));
 // magneticLinks.forEach(elem => elem.addEventListener("mousemove", magnetize));
