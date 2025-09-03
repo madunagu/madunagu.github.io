1f881a42b55af43b2030e13adbe26a9997c667b6
Ekene Madunagu <ekenemadunagu@gmail.com>
2025-09-03 22:49:30 +0100

    fix links

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        portfolio: resolve(__dirname, 'portfolio.html')
+      }
+    }
+  }
   // base: '/dos/', 
 })
